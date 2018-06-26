5699319ee43b934792ecd57f03cae8af53b88d63
Dexter Burk <dexterburk@dexters-air.fios-router.home>
2018-06-26 16:41:16 -0400

    Initial Commit

---
+++ Land/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Land
+//
+//  Created by Dexter Burk on 6/26/18.
+//  Copyright © 2018 Dexter Burk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
